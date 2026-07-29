4444a1a62e7c40d46af100837454ff5982f02cd9
wxrtezfnz-lgtm <wxrtezfnz@gmail.com>
2026-07-29 23:04:02 +0300

    fix dispatcher initialization

--- app/bot.py
+++ app/bot.py
@@ -1,3 +1,7 @@
+from aiogram import Bot, Dispatcher
+
+from app.config import BOT_TOKEN
+
 from app.handlers import (
     start,
     earnings,
@@ -7,8 +11,32 @@ from app.handlers import (
 )
 
 
-dp.include_router(start.router)
-dp.include_router(earnings.router)
-dp.include_router(design.router)
-dp.include_router(courses.router)
-dp.include_router(profile.router)
+bot = Bot(
+    token=BOT_TOKEN
+)
+
+
+dp = Dispatcher()
+
+
+# Регистрация роутеров
+
+dp.include_router(
+    start.router
+)
+
+dp.include_router(
+    earnings.router
+)
+
+dp.include_router(
+    design.router
+)
+
+dp.include_router(
+    courses.router
+)
+
+dp.include_router(
+    profile.router
+)
