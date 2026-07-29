595b474254b4a550f6fe882fe195c4e7f17bd172
wxrtezfnz-lgtm <wxrtezfnz@gmail.com>
2026-07-29 11:56:45 +0300

    fix earnings keyboard

--- app/keyboards/earnings.py
+++ app/keyboards/earnings.py
@@ -1,38 +1,38 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-def earnings_categories_keyboard():
-
+def earnings_keyboard():
     return InlineKeyboardMarkup(
         inline_keyboard=[
-
             [
                 InlineKeyboardButton(
                     text="🔥 Фриланс",
                     callback_data="earn_freelance"
                 )
             ],
-
             [
                 InlineKeyboardButton(
                     text="📱 Онлайн-проекты",
                     callback_data="earn_online"
                 )
             ],
-
             [
                 InlineKeyboardButton(
                     text="🛒 Бизнес",
                     callback_data="earn_business"
                 )
             ],
-
             [
                 InlineKeyboardButton(
                     text="📈 Инвестиции",
                     callback_data="earn_invest"
                 )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="⬅️ Назад",
+                    callback_data="back_main"
+                )
             ]
-
         ]
     )
